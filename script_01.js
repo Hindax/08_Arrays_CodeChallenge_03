e44fe897fbac0166672f2b0e0528e9ffaced49f3
Hindax <hind.aarif@gmail.com>
2022-11-16 23:30:57 +0100

    slt

--- script_01.js
+++ script_01.js
@@ -12,7 +12,25 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-output(getSentence(["Ich","bin","Max"],"S"));
-output(getSentence(["Bist","du","Max"],"Q"));
-output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,"," habbe","Sie","die"],"Q"));
+// output(getSentence(["Ich","bin","Max"],"S"));
+// output(getSentence(["Bist","du","Max"],"Q"));
+// output(getSentence(["Ich","bin"],"E"));
+// output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,"," habbe","Sie","die"],"Q"));
+
+
+output(getSentence( [ "Ich","bin","Max"],"S"));
+output(getSentence( [ "Bist","du","Max"],"Q"));
+output(getSentence( [ "Ich","bin"],"E"));
+output(getSentence( [ "Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+
+function output( outputStr1 ) { console.log( outputStr1 ); }
+    // get the strings
+function getSentence( iArr, iSng ) {
+    let iArrStr = "";
+    let iSngStr = ( iSng == "Q" ) ? "?" : ( iSng == "E" ) ? "!" : ".";
+
+    for ( let i = 0; i < iArr.length; i ++ ) {
+        ( i < iArr.length - 1 ) ? iArrStr += ( iArr[ i ] + " " ) : iArrStr += iArr[ i ];
+    }
+    return iArrStr + iSngStr;
+}
